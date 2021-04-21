9f2b27844ff51628b8e62a6464349fba179e66b0
Oren Epshtain <oepshtain@infinidat.com>
2021-04-21 15:07:16 +0300

    Add wait groups

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 /*
@@ -15,6 +15,12 @@ The advantage of this abstraction is that go-routines can start with very small 
 very quickly, and thus they are very cheap to create and to destroy.
 This abstraction lets us to use threads without the fear of creating or destroying them.
 */
+
+var wg = sync.WaitGroup{}
+
+// waitgroup is designed to synchronize multiple go-routines together.
+// Here we have 2 go-routines: the one that executes the main function and the go-routine
+// we're spawning off with the msg. We want to synchronize the main function to the go-routine.
 
 func main() {
 	// go sayHello() // "go" turns a function into a go routine
@@ -29,11 +35,19 @@ func main() {
 	// It is generally not advised to access a varaible that's defined in the outer scope of a function
 	// inside the function, especially with go-routines which can lead to race conditions like so:
 	var msg = "Hello"
+	wg.Add(1) // starts off synchronizing to zero, so we add 1 telling it that we want to
+	// synchronize to this go-routine right here below.
 	go func(msg string) {
 		fmt.Println(msg)
+		// when the go-routine is done it can tell the waitgroup that it's actually completed
+		// its execution, by the .Done() method. (will decrement the number of groups the waiting group
+		// is waiting on, since we added 1, it will be back at 0, and then the .Wait() method will know that
+		// it's time to finish our application run)
+		wg.Done()
 	}(msg)
 	msg = "Goodbye"
-	time.Sleep(100 * time.Millisecond)
+	wg.Wait()
+	// time.Sleep(100 * time.Millisecond)
 	// Most of the time we will see "Goodbye" printed here, because the go scheduler will not interupt
 	// the main thread until it hits the sleep call. Even though it launches another go-routine at the start
 	// it didn't give it any "love" yet, it still is executing the main function. So it reassigns the msg variable
